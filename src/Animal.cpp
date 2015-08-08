dac5ff992b65d3980d830df60058cff4c45aa678
John Law <klaw428@aucklanduni.ac.nz>
2015-08-08 21:03:21 +1200

    added subscriber to animal

--- src/Animal.cpp
+++ src/Animal.cpp
@@ -13,10 +13,13 @@ Animal::~Animal() {
 }
 
 Animal alphaDog;
-geometry_msgs::Twist RobotNode_cmdvel;
 
 void stage_callback(nav_msgs::Odometry msg) {
     alphaDog.stageOdom_callback(msg);
+    int x = msg.pose.pose.position.x;
+	int y = msg.pose.pose.position.y;
+    alphaDog.setPose(x,y,0);
+
 }
 
 
@@ -30,27 +33,21 @@ int main(int argc, char **argv)
     //create ros handler for this node
     ros::NodeHandle n;
     
-  //  alphaDog.robotNode_stage_pub = n.advertise<geometry_msgs::Twist>("cmd_vel",1000);
-    ros::Publisher RobotNode_stage_pub = n.advertise<geometry_msgs::Twist>("cmd_vel",1000);
-    //    ros::Publisher cmd_pub = n.advertise<geometry_msgs::Twist>("cmd_vel",1);
+    alphaDog.robotNode_stage_pub = n.advertise<geometry_msgs::Twist>("cmd_vel",1000);
+  
 
-
-//    alphaDog.stageOdo_Sub = n.subscribe<nav_msgs::Odometry>("alphaDog/odom",1000,stage_callback);
+    alphaDog.stageOdo_Sub = n.subscribe<nav_msgs::Odometry>("odom",1000,stage_callback);
     
     ros::Rate loop_rate(10); 
 
     while (ros::ok())
     {
         //message to stage
-  //      alphaDog.setVelocity(0.2,0.2);
-        //alphaDog.updateOdometry();
+        alphaDog.setVelocity(0.2,0.2);
+        alphaDog.updateOdometry();
         
-        RobotNode_cmdvel.linear.x = 1000000000;
-        RobotNode_cmdvel.linear.y = 0;
-        RobotNode_cmdvel.angular.z = 1000000000;
-       // alphaDog.robotNode_stage_pub.publish(RobotNode_cmdvel);
-        RobotNode_stage_pub.publish(RobotNode_cmdvel);
-       // ros::spinOnce();
+
+        ros::spinOnce();
     
         loop_rate.sleep();
 
