143208b9bea782d8eb92a9c857d929cf0d61453b
John Law <klaw428@aucklanduni.ac.nz>
2015-08-18 00:09:43 +1200

    fixed return 0 in while loop and also pass string argument for the beacon publisher topic to be unique instead of /robot_.....

--- src/Beacon.cpp
+++ src/Beacon.cpp
@@ -29,15 +29,20 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
     
     beacon.robotNode_stage_pub = n.advertise<geometry_msgs::Twist>("cmd_vel",1000);
+    ros::Publisher robotNode_location_pub = n.advertise<nav_msgs::Odometry>(argv[1],1000);
     beacon.stageOdo_Sub = n.subscribe<nav_msgs::Odometry>("base_pose_ground_truth",1000,stage_callback);
 
     ros::Rate loop_rate(10); 
-	
+	nav_msgs::Odometry tempMessage; 
     while (ros::ok())
     {
-        
+        tempMessage.pose.pose.position.x = beacon.getX();
+        tempMessage.pose.pose.position.y = beacon.getY();  
+        robotNode_location_pub.publish(tempMessage);
+    
 	    ros::spinOnce();
         loop_rate.sleep();
-        return 0;
+        
     }
+    return 0;
 }
