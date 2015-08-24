455b9ea40f5aba1c90c2921c2f36465a015b3d5b
Elliot Whiley <elliot.w@hotmail.com>
2015-08-24 20:22:58 +1200

    Robot class formatting

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -2,15 +2,14 @@
 #include <nav_msgs/Odometry.h>
 
 Robot::Robot():Entity(0,0,0,0,0) {
-	// TODO Auto-generated constructor stub
+
 }
 
-Robot::Robot(double x,double y,double theta,double linearVel, double angularVel)
-	:Entity(x, y, theta, linearVel, angularVel){
+Robot::Robot(double x,double y,double theta,double linearVel, double angularVel) : Entity(x, y, theta, linearVel, angularVel) {
 }
 
 Robot::~Robot() {
-	// TODO Auto-generated destructor stub
+
 }
 
 Robot::State Robot::getState() {
