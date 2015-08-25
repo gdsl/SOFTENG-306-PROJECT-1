dd08cc887ed4de93f578e0557f54e1e448603c5b
lyuraurora <lyuraurora@gmail.com>
2015-08-26 10:39:45 +1200

    Delete some useless thing and add comment

--- src/Neighbour.cpp
+++ src/Neighbour.cpp
@@ -27,24 +27,8 @@ Neighbour::Neighbour(double x, double y, double theta, double linearVelocity, do
 }
 
 Neighbour neighbour;
-// Default cat behaviour = walking
+// Default cat behaviour = Idle
 std::string status="Idle";
-bool queueFull = false;
-
-// Keeps track of current position that cat is facing
-double radians;
-double angle;
-bool initial = true;
-/**
- * Represents FSM for Neighbour. Given an action, update the current status
- */
-/*void Neighbour::next(std::string action) {
-	std::string currentStatus = getStatus();
-
-	//if (currentStatus =="WALKING")ccc{
-	//if (
-	//}
-}*/
 
 
 void Neighbour::stageOdom_callback(const nav_msgs::Odometry msg) {
@@ -113,7 +97,6 @@ int main(int argc, char **argv) {
 	neighbour.setOriginX(xPos);
 	neighbour.robotNode_stage_pub = n.advertise<geometry_msgs::Twist>("cmd_vel",1000);
 	neighbour.stageOdo_Sub = n.subscribe<nav_msgs::Odometry>("base_pose_ground_truth", 1000, &Neighbour::stageOdom_callback, &neighbour);
-	//Neighbour.baseScan_Sub = n.subscribe<sensor_msgs::LaserScan>("base_scan", 1000, &Neighbour::stageLaser_callback, &Neighbour);
 	neighbour.Neighbour_status_pub = n.advertise<se306project::robot_status>("status",1000);
 	//subscribe to laser
 	neighbour.baseScan_Sub = n.subscribe<sensor_msgs::LaserScan>("base_scan", 1000,callBackLaserScan);
@@ -126,20 +109,13 @@ int main(int argc, char **argv) {
 	neighbour.addMovement("forward_x", -35, 1);
 	// ROS infinite loop
 	while (ros::ok()) {
-                // Message to stage
-		//neighbour.faceWest(1);
-                //neighbour.addMovement("forward_x", -35, 1);
-                //neighbour.move();
-                //neighbour.setStatus("Moving to a robot");
-
-		//publish message
 
                 ros::spinOnce();
 		loop_rate.sleep();
 
 		if (neighbour.getMovementQueueSize() == 0){
 			if(neighbour.getStatus().compare("Finding a robot")==0){
-				neighbour.setStatus("Moving back from a robot");
+				neighbour.setStatus("Moving back ");
 				neighbour.faceEast(1);
                 neighbour.addMovement("forward_x", neighbour.getOriginX()-neighbour.getX(), 1);
 			}else{
@@ -155,7 +131,6 @@ int main(int argc, char **argv) {
 		status_msg.pos_theta = neighbour.getTheta();
 		status_msg.status = neighbour.getStatus();
 		status_msg.obstacle = neighbour.getObstacleStatus();
-		neighbour.Neighbour_status_pub.publish(status_msg);
-	                //neighbour.determineStatus();              
+		neighbour.Neighbour_status_pub.publish(status_msg);             
 	}
 }
