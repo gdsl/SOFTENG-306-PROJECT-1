6997bd03d123bd5f93f1b46a8cb76264023e958d
wyep266 <wesleyyep@hotmail.com>
2015-08-27 10:15:30 +1200

    modified tractor

--- src/Tractor.cpp
+++ src/Tractor.cpp
@@ -2,6 +2,7 @@
 #include "std_msgs/String.h"
 #include "Tractor.h"
 #include <sstream>
+#include "se306project/robot_status.h"
 
 /*
  * Default constructor for carrier Robot
@@ -80,20 +81,20 @@ int main(int argc, char **argv) {
 	oss << "robot_" << xString.c_str() << "/cmd_vel";
 	tractor.robotNode_stage_pub = n.advertise<geometry_msgs::Twist>(oss.str().c_str(),1000);
 	// Broadcast the node's status information for other to subscribe to.
-	// ros::Publisher pub=n.advertise<se306project::carrier_status>("status",1000);
+//	 ros::Publisher pub=n.advertise<se306project::robot_status>("/robot_27/status",1000);
 
 	// Subscribe to listen to messages coming from stage for odometry (this is base pose so it is
 	// relative to the absolute frame of the farm.
-	tractor.stageOdo_Sub = n.subscribe<nav_msgs::Odometry>("base_pose_ground_truth",1000, callBackStageOdm);
+//	tractor.stageOdo_Sub = n.subscribe<nav_msgs::Odometry>("/robot_27/base_pose_ground_truth",1000, callBackStageOdm);
 
 	// Subscribe to topic for manual control
 	// ros::Subscriber mysub_object = n.subscribe("/tractor",100,receiveTractorControl);
 
 	// A count of how many messages we have sent
-	// int count = 0;
+	 int count = 0;
 
 	// Carrier status message initialisation
-	// se306project::carrier_status status_msg;
+	 se306project::robot_status status_msg;
 
 	std::string buffer;
 	while(std::cin) {
@@ -116,26 +117,35 @@ int main(int argc, char **argv) {
 			tractor.setLin(0);
 			tractor.setAng(0);
 		}
-		tractor.updateOdometry();
+		
+        tractor.updateOdometry();
+
+		status_msg.my_counter=count;		//add counter to message
+		status_msg.status="driving";		//add status to message
+		status_msg.pos_x=tractor.getX(); //add x to message to broadcast
+		status_msg.pos_y=tractor.getY();//add y to message to broadcast
+		status_msg.pos_theta=tractor.getTheta(); //add angle to message to broadcast
+		status_msg.obstacle = "none";
+	//	pub.publish(status_msg);	//publish message
+        
+        count++;
 		ros::spinOnce();
 	}
 
+    
+/*
 	// ROS infinite loop
 	while (ros::ok())
 	{
 		ros::spinOnce();
-		//status_msg.my_counter=count;		//add counter to message
-		//status_msg.status=carrierRobot.getStatus();		//add status to message
-		//status_msg.pos_x=carrierRobot.getX(); //add x to message to broadcast
-		//status_msg.pos_y=carrierRobot.getY();//add y to message to broadcast
-		//status_msg.pos_theta=carrierRobot.getTheta(); //add angle to message to broadcast
-		//status_msg.obstacle = obstacleStatus;
-		//pub.publish(status_msg);	//publish message
-		//carrierRobot.stateLogic();
+
+
 		tractor.updateOdometry();
 		loop_rate.sleep();
 		//++count; // increase counter
 	}
+*/
+
 	return 0;
 }
 
