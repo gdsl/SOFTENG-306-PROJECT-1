686caea84529a357ebc53805245e8f8772be2b0e
Yeu-Shin (Guyver) Fu <yfu959@aucklanduni.ac.nz>
2015-08-27 08:42:32 +1200

    added in dynamic process angle for entities

--- src/Person.cpp
+++ src/Person.cpp
@@ -1,10 +1,11 @@
 #include "Person.h"
+#include "Constants.h"
 
 Person::Person():Entity() {
 
 }
 
-Person::Person(double x, double y):Entity(x,y,0,0,0) {
+Person::Person(double x, double y):Entity(x,y,0,0,0,HUMAN_ANGLE_NOT_PROCESS) {
 
 }
 
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,11 +1,12 @@
 #include "Robot.h"
 #include <nav_msgs/Odometry.h>
+#include "Constants.h" 
 
-Robot::Robot():Entity(0,0,0,0,0) {
+Robot::Robot():Entity(0,0,0,0,0,0) {
 
 }
 
-Robot::Robot(double x,double y,double theta,double linearVel, double angularVel) : Entity(x, y, theta, linearVel, angularVel) {
+Robot::Robot(double x,double y,double theta,double linearVel, double angularVel) : Entity(x, y, theta, linearVel, angularVel,ROBOT_ANGLE_NOT_PROCESS) {
 
 }
 
